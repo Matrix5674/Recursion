e9f9ac816bd3f945b2170a0e616dffdb8ad88773
Soham Sahasrabuddhe <sohamsah28@gmail.com>
2025-01-18 10:51:11 -0800

    done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(insertStarV2("Hello"));
         System.out.println(countX("Hexlxosxxo"));
+        System.out.println(isPallindrome("hello"));
+        System.out.println(removeAll("hellxxoxoxoxooooxooxooox", "x"));
+        System.out.println(groupSum(0, new int[] {1, 2, 3, 4, 5, 6, 7, 8}, 16));
     }
 
     public static String insertStarV1 (String str) {
@@ -32,11 +35,29 @@ public class Main {
         if (str.length() <= 1){
             return true;
         }
+        boolean checkIfFirstAndLastMatch = (str.substring(0, 1).equals(str.substring(str.length()-1)));
+        return checkIfFirstAndLastMatch && isPallindrome(str.substring(1, str.length()-1));
 
     }
 
-    public static int isPallindromeHelper (String str){
-        if (str.length() <= 1) return 1;
-        return 1 + isPallindromeHelper()
+    public static String removeAll (String str, String x) {
+        if (x.length() > 1) throw new IllegalArgumentException(); //Input validator. Pretend x is of CHAR type.
+        if (str.isEmpty()) return "";
+
+        if (str.substring(0, 1).equals(x)) {
+            return removeAll(str.substring(1), x);
+        }
+        else {
+            return str.substring(0, 1) + removeAll(str.substring(1), x);
+        }
+
     }
+
+    public static boolean groupSum(int startIndex, int[] arr, int targetSum){
+        if (startIndex >= arr.length)
+            if (targetSum == 0) return true;
+
+        return groupSum(startIndex+1, arr, targetSum-arr[0]) || groupSum(startIndex + 1, arr, targetSum);
+    }
+
 }
